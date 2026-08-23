6f7f231aaefa0d177010b8c4a6c8cb3b6386af4b
Almas <almasakbor222@gamil.com>
2026-08-23 21:29:22 +0600

    Bakery Management

---
+++ BakeryManagement/BakeryManagement/Purchase.Designer.cs
@@ -0,0 +1,312 @@
+﻿
+namespace BakeryManagement
+{
+    partial class Purchase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.buttonlogout = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.payment = new System.Windows.Forms.Button();
+            this.show = new System.Windows.Forms.Button();
+            this.pquan = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.add = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.pname = new System.Windows.Forms.TextBox();
+            this.pprice = new System.Windows.Forms.Label();
+            this.pricetxt = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::BakeryManagement.Properties.Resources.Gemini_Generated_Image_d615jad615jad615;
+            this.pictureBox2.Location = new System.Drawing.Point(315, 2);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1162, 196);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 35;
+            this.pictureBox2.TabStop = false;
+            // 
+            // buttonlogout
+            // 
+            this.buttonlogout.BackColor = System.Drawing.Color.Tomato;
+            this.buttonlogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonlogout.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonlogout.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonlogout.Location = new System.Drawing.Point(4, 593);
+            this.buttonlogout.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonlogout.Name = "buttonlogout";
+            this.buttonlogout.Size = new System.Drawing.Size(301, 62);
+            this.buttonlogout.TabIndex = 5;
+            this.buttonlogout.Text = "Exit";
+            this.buttonlogout.UseVisualStyleBackColor = false;
+            this.buttonlogout.Click += new System.EventHandler(this.buttonlogout_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 119);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Customer Dashboard";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BakeryManagement.Properties.Resources.home;
+            this.pictureBox1.Location = new System.Drawing.Point(121, 76);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(67, 39);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Bisque;
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.buttonlogout);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(309, 660);
+            this.panel1.TabIndex = 34;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(0, 207);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(307, 70);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Purchase";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // payment
+            // 
+            this.payment.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.payment.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.payment.Location = new System.Drawing.Point(931, 269);
+            this.payment.Margin = new System.Windows.Forms.Padding(4);
+            this.payment.Name = "payment";
+            this.payment.Size = new System.Drawing.Size(546, 34);
+            this.payment.TabIndex = 11;
+            this.payment.Text = "Payment";
+            this.payment.UseVisualStyleBackColor = true;
+            this.payment.Click += new System.EventHandler(this.payment_Click);
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(674, 220);
+            this.show.Margin = new System.Windows.Forms.Padding(4);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(175, 137);
+            this.show.TabIndex = 51;
+            this.show.Text = "Show";
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Click += new System.EventHandler(this.show_Click);
+            // 
+            // pquan
+            // 
+            this.pquan.Location = new System.Drawing.Point(485, 258);
+            this.pquan.Margin = new System.Windows.Forms.Padding(4);
+            this.pquan.Name = "pquan";
+            this.pquan.Size = new System.Drawing.Size(132, 22);
+            this.pquan.TabIndex = 47;
+            this.pquan.TextChanged += new System.EventHandler(this.pquan_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(349, 258);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 46;
+            this.label3.Text = "Quantity :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(349, 223);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 17);
+            this.label2.TabIndex = 45;
+            this.label2.Text = "Prodect Name : ";
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(384, 337);
+            this.add.Margin = new System.Windows.Forms.Padding(4);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(235, 28);
+            this.add.TabIndex = 44;
+            this.add.Text = "Add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(335, 373);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(544, 274);
+            this.dataGridView1.TabIndex = 43;
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(931, 373);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.Size = new System.Drawing.Size(540, 274);
+            this.dataGridView2.TabIndex = 56;
+            // 
+            // pname
+            // 
+            this.pname.Location = new System.Drawing.Point(485, 220);
+            this.pname.Margin = new System.Windows.Forms.Padding(4);
+            this.pname.Name = "pname";
+            this.pname.Size = new System.Drawing.Size(132, 22);
+            this.pname.TabIndex = 57;
+            // 
+            // pprice
+            // 
+            this.pprice.AutoSize = true;
+            this.pprice.Location = new System.Drawing.Point(349, 290);
+            this.pprice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.pprice.Name = "pprice";
+            this.pprice.Size = new System.Drawing.Size(40, 17);
+            this.pprice.TabIndex = 58;
+            this.pprice.Text = "Price";
+            // 
+            // pricetxt
+            // 
+            this.pricetxt.Location = new System.Drawing.Point(485, 290);
+            this.pricetxt.Margin = new System.Windows.Forms.Padding(4);
+            this.pricetxt.Name = "pricetxt";
+            this.pricetxt.Size = new System.Drawing.Size(132, 22);
+            this.pricetxt.TabIndex = 59;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::BakeryManagement.Properties.Resources.back_button;
+            this.pictureBox3.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(51, 37);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // Purchase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1483, 667);
+            this.Controls.Add(this.pricetxt);
+            this.Controls.Add(this.pprice);
+            this.Controls.Add(this.pname);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.payment);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.pquan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Purchase";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Purchase";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button buttonlogout;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button payment;
+        private System.Windows.Forms.Button show;
+        private System.Windows.Forms.TextBox pquan;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox pname;
+        private System.Windows.Forms.Label pprice;
+        private System.Windows.Forms.TextBox pricetxt;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
